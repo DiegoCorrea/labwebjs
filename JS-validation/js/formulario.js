736dd2e82e2db3f9a63d663de79016adea52347b
diegocorrea <diegocorrea.cc@gmail.com>
2016-08-12 14:25:58 -0300

    stable version

--- JS-validation/js/formulario.js
+++ JS-validation/js/formulario.js
@@ -94,13 +94,33 @@ $(document).ready(function(){
     }
   });
 });
-
+jQuery(function($){
+  $.datepicker.regional['pt-BR'] = {
+    closeText: 'Fechar',
+    prevText: '&#x3c;Anterior',
+    nextText: 'Pr&oacute;ximo&#x3e;',
+    currentText: 'Hoje',
+    monthNames: ['Janeiro','Fevereiro','Mar&ccedil;o','Abril','Maio','Junho',
+    'Julho','Agosto','Setembro','Outubro','Novembro','Dezembro'],
+    monthNamesShort: ['Jan','Fev','Mar','Abr','Mai','Jun',
+    'Jul','Ago','Set','Out','Nov','Dez'],
+    dayNames: ['Domingo','Segunda-feira','Ter&ccedil;a-feira','Quarta-feira','Quinta-feira','Sexta-feira','Sabado'],
+    dayNamesShort: ['Dom','Seg','Ter','Qua','Qui','Sex','Sab'],
+    dayNamesMin: ['Dom','Seg','Ter','Qua','Qui','Sex','Sab'],
+    weekHeader: 'Sm',
+    dateFormat: 'dd/mm/yy',
+    firstDay: 0,
+    isRTL: false,
+    changeYear: true,
+    yearRange: '1950:2013',
+    showMonthAfterYear: false,
+    yearSuffix: ''};
+  $.datepicker.setDefaults($.datepicker.regional['pt-BR']);
+});
 //Jquery para entrada de datas
 $(document).ready(function(){
-  $( "#datepicker" ).datepicker();
+  $( "#datepicker" ).datepicker($.datepicker.regional["pt-BR"]);
 });
-
-
 
 //Botão de 
 $(document).ready(function(){
