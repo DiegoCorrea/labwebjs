64fe54e35f080f83b4d3aac5b624729ea594e398
diegocorrea <diegocorrea.cc@gmail.com>
2016-09-15 03:35:18 -0300

    start layout

---
+++ RAILS/VIP/app/assets/javascripts/peoples.js
@@ -0,0 +1,36 @@
+$(document).ready(function(){
+  $('#cellPhone').mask("(00) 0 0000-0000");
+});
+
+$(document).ready(function(){
+  $('#CPF').mask("000.000.000-00");
+});
+
+jQuery(function($){
+  $.datepicker.regional['pt-BR'] = {
+    closeText: 'Fechar',
+    prevText: '&#x3c;Anterior',
+    nextText: 'Pr&oacute;ximo&#x3e;',
+    currentText: 'Hoje',
+    monthNames: ['Janeiro','Fevereiro','Mar&ccedil;o','Abril','Maio','Junho',
+    'Julho','Agosto','Setembro','Outubro','Novembro','Dezembro'],
+    monthNamesShort: ['Jan','Fev','Mar','Abr','Mai','Jun',
+    'Jul','Ago','Set','Out','Nov','Dez'],
+    dayNames: ['Domingo','Segunda-feira','Ter&ccedil;a-feira','Quarta-feira','Quinta-feira','Sexta-feira','Sabado'],
+    dayNamesShort: ['Dom','Seg','Ter','Qua','Qui','Sex','Sab'],
+    dayNamesMin: ['Dom','Seg','Ter','Qua','Qui','Sex','Sab'],
+    weekHeader: 'Sm',
+    dateFormat: 'dd/mm/yy',
+    firstDay: 0,
+    isRTL: false,
+    changeMonth: true,
+    changeYear: true,
+    yearRange: '1950:2013',
+    showMonthAfterYear: false,
+    yearSuffix: ''};
+  $.datepicker.setDefaults($.datepicker.regional['pt-BR']);
+});
+//Jquery para entrada de datas
+$(document).ready(function(){
+  $( "#borned" ).datepicker($.datepicker.regional["pt-BR"]);
+});
--- RAILS/VIP/config/routes.rb
+++ RAILS/VIP/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'peoples#index'
   resources :peoples
-  post "peoples/[:id]" => "peoples#update"
+  post "peoples/:id/edit" => "peoples#update"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
